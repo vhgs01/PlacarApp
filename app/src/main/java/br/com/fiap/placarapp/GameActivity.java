658a849a6a4e34c5b4600b60d14c9df10d520df6
logonrm <logonrm@fiap.com.br>
2017-12-04 23:06:21 -0200

    Initial commit

---
+++ app/src/main/java/br/com/fiap/placarapp/GameActivity.java
@@ -0,0 +1,31 @@
+package br.com.fiap.placarapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class GameActivity extends AppCompatActivity {
+
+    private String timeCasa;
+    private String timeVisitante;
+
+    private TextView tvTimeCasa;
+    private TextView tvTimeVisitante;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        tvTimeCasa = (TextView) findViewById(R.id.etTimeCasa);
+        tvTimeVisitante = (TextView) findViewById(R.id.edTimeVisitante);
+
+        if (getIntent() != null) {
+            timeCasa = getIntent().getStringExtra("TIMECASA");
+            timeVisitante = getIntent().getStringExtra("TIMEVISITANTE");
+
+            tvTimeCasa.setText(timeCasa);
+            tvTimeVisitante.setText(timeVisitante);
+        }
+    }
+}
