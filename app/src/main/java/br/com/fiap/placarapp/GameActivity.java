6534bd31c56853eb3ce81bbf18fb337260b86294
logonrm <logonrm@fiap.com.br>
2017-12-06 22:04:54 -0200

    Criacao da tela placar e suas funcoes de click

--- app/src/main/java/br/com/fiap/placarapp/GameActivity.java
+++ app/src/main/java/br/com/fiap/placarapp/GameActivity.java
@@ -2,30 +2,80 @@ package br.com.fiap.placarapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
 
+    //    DECLARAÇÃO DE VARIÁVEIS LOCAIS
     private String timeCasa;
     private String timeVisitante;
+    private int contadorCasa;
+    private int contadorVisitante;
 
+    //    DECLARAÇÃO DE VARIÁVEIS DE CADA TIPO
     private TextView tvTimeCasa;
     private TextView tvTimeVisitante;
+    private TextView tvContadorCasa;
+    private TextView tvContadorVisitante;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+//        REFERENCIANDO UM ELEMENTO BASEADO-SE NO SEU ID
         tvTimeCasa = (TextView) findViewById(R.id.tvTimeCasa);
         tvTimeVisitante = (TextView) findViewById(R.id.tvTimeVisitante);
+        tvContadorCasa = (TextView) findViewById(R.id.tvContadorGolCasa);
+        tvContadorVisitante = (TextView) findViewById(R.id.tvContadorGolVisitante);
 
+//        CASO VENHA VALORES DE OUTRA INTENT (VIEW) ELE ATRIBUI A VARIÁVEL LOCAL
         if (getIntent() != null) {
+
+//            ATRIBUI A VARIÁVEIS OS VALORES JÁ EXISTENTES NA INTENT, ATRAVÉS DE SEUS RESPECTIVOS NOMES
             timeCasa = getIntent().getStringExtra("TIMECASA");
             timeVisitante = getIntent().getStringExtra("TIMEVISITANTE");
 
+//            SETANDO VALORES NO ELEMENTO NA TELA
             tvTimeCasa.setText(timeCasa);
             tvTimeVisitante.setText(timeVisitante);
         }
+
+//        VERIFICA SE TEM ALGUM VALOR SALVO NO STATUS DA INSTÂNCIA
+        if (savedInstanceState != null) {
+
+//            ATRIBUI A VARIÁVEIS OS VALORES EXISTENTES NO STATUS DA INSTÂNCIA
+            contadorCasa = savedInstanceState.getInt("CONTADORCASA");
+            contadorVisitante = savedInstanceState.getInt("CONTADORVISITANTE");
+
+//            SETANDO VALORES NO ELEMENTO NA TELA
+            tvContadorCasa.setText(String.valueOf(contadorCasa));
+            tvContadorVisitante.setText(String.valueOf(contadorVisitante));
+        }
+    }
+
+    //    FUNÇÃO DO ONCLICK DO BOTAO GOL CASA
+    public void golCasa(View v) {
+        contadorCasa++;
+//            SETANDO VALORES NO ELEMENTO NA TELA
+        tvContadorCasa.setText(String.valueOf(contadorCasa));
+    }
+
+    //    FUNÇÃO DO ONCLICK DO BOTAO GOL VISITANTE
+    public void golVisitante(View v) {
+        contadorVisitante++;
+//            SETANDO VALORES NO ELEMENTO NA TELA
+        tvContadorVisitante.setText(String.valueOf(contadorVisitante));
+    }
+
+    //    MÉTODO QUE SALVA VARIÁVEIS NA STATUS DA INSTÂNCIA
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+//        ATRIBUI VALORES NOS ELEMENTOS DA INSTÂNCIA
+        outState.putInt("CONTADORCASA", contadorCasa);
+        outState.putInt("CONTADORVISITANTE", contadorVisitante);
     }
 }
--- app/src/main/java/br/com/fiap/placarapp/MainActivity.java
+++ app/src/main/java/br/com/fiap/placarapp/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    //    DECLARAÇÃO DE VARIÁVEIS DE CADA TIPO
     private EditText etTimeCasa;
     private EditText etTimeVisitante;
 
@@ -17,25 +18,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        REFERENCIANDO UM ELEMENTO BASEADO-SE NO SEU ID
         etTimeCasa = (EditText) findViewById(R.id.etTimeCasa);
         etTimeVisitante = (EditText) findViewById(R.id.edTimeVisitante);
     }
 
+    //    FUNÇÃO DO ONCLICK DO BOTAO COMEÇAR A JOGAR
     public void comecarJogo(View v) {
 
+//        VERIFICA SE OS CAMPOS ESTÃO VAZIOS OU EM BRANCO
         if (etTimeCasa.getText().toString().isEmpty()) {
+
+//            EXIBE UMA MENSAGEM PRO USUÁIO
             Toast.makeText(this, "Informe o time da casa", Toast.LENGTH_SHORT).show();
             return;
         }
 
+//        VERIFICA SE OS CAMPOS ESTÃO VAZIOS OU EM BRANCO
         if (etTimeVisitante.getText().toString().isEmpty()) {
+//            EXIBE UMA MENSAGEM PRO USUÁIO
             Toast.makeText(this, "Informe o time visitante", Toast.LENGTH_SHORT).show();
             return;
         }
 
+//        CRIA UMA INTENT REFERENCIANDO ESSE ESCOPO E A NOVA TELA A SER CHAMADA
         Intent proxTela = new Intent(this, GameActivity.class);
+
+//        SETA VALORES A SEREM PASSADOS PARA A NOVA TELA
         proxTela.putExtra("TIMECASA", etTimeCasa.getText().toString());
         proxTela.putExtra("TIMEVISITANTE", etTimeVisitante.getText().toString());
+
+//        CHAMA A PRÓXIMA TELA
         startActivity(proxTela);
     }
 }
